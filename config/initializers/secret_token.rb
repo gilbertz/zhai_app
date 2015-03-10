bfaa4bbeb7981cadec893bdae1222f7894c3aee8
gilbertz <gilbertzhaougoqi@hotmail.com>
2015-03-10 09:39:34 +0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ZhaiApp::Application.config.secret_key_base = '607f33205aae80c76b5936d770fd42163d63a7e750b7606d9676ef23116a37f8b227af96fc5e128a81cd374f371168c6b7f76fef18bc823c2a304f8d62595090'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ZhaiApp::Application.config.session_store :cookie_store, key: '_zhai_app_session'
